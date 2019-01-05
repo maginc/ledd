d95705476165404cc9e4768c25b81dc95021e6ce
Your Name <you@example.com>
2019-01-05 14:45:42 +0200

    ss

--- src/main/java/com/ragazm/ledd/Application.kt
+++ src/main/java/com/ragazm/ledd/Application.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class Application
+class Application
 
     fun main(args: Array<String>) {
         runApplication<Application>(*args)
--- src/main/java/com/ragazm/ledd/controller/LEDController.kt
+++ src/main/java/com/ragazm/ledd/controller/LEDController.kt
@@ -1,15 +1,23 @@
 package com.ragazm.ledd.controller
 
 import com.pi4j.io.gpio.*
+import com.ragazm.ledd.sensors.BME280
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class LEDController {
 
-    @RequestMapping("/")
-    fun greeting(): String {
-        return "Hello Bitch!!"
+   // @RequestMapping("/")
+    //fun greeting() = "index"
+
+
+    @RequestMapping("/temperature")
+    fun temperature(): String {
+        var doubleTemp = BME280.sensor()["Temperature"]
+        var doubleHumidity = BME280.sensor()["Humidity"]
+
+        return doubleTemp.toString() + "<br>" + doubleHumidity.toString()
     }
 
     @RequestMapping("/light")
@@ -23,7 +31,6 @@ class LEDController {
     }
 
     companion object {
-
         var pin: GpioPinDigitalOutput? = null
     }
 
